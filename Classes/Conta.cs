5753c6c456142cd7d2fa97db7d0af7eaa4169497
FilipeLageGarcia <filipelagegarcia@gmail.com>
2021-03-08 03:26:10 -0300

    adicionado enum e outras coisas

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -5,6 +5,15 @@ namespace DIO.Bank
         private TipoConta TipoConta { get; set; }
         private double Saldo { get; set; }
         private double Credito { get; set; }
-        private double Nome { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = TipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+        
     }
 }
