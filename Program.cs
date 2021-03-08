75eee5e5c4dde2d2bd136419ce94355fb26f62e3
FilipeLageGarcia <filipelagegarcia@gmail.com>
2021-03-08 03:13:39 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaconta = new Conta();
+            minhaconta.Nome = "Filipe";
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
